b5842e95b69b46882d2c7012a1fdb5b1360748b5
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-05-16 14:25:43 +0700

    Remove couple of tests

--- test/Audio/Volume.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useMediaContext } from '../../src';
-
-export const Volume = () => {
-  const { toggleMuted } = useMediaContext();
-  return <button onClick={toggleMuted}>Toggle Muted</button>;
-};
